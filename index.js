bf869b8133db0780b930bb17dddc6d86363a8dd1
andersonmguedes <andersonm.guedes@gmail.com>
2021-08-25 01:40:41 -0300

    Servidor Node

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const http =require('http');
+//const http =require('http');
 
-http.createServer((req,res)=>{}).listen(3000)
+//http.createServer((req,res)=>{
+  //  res.end('Helo Word!!!! Doug Funny');
+//}).listen(3000)
 
-console.log('Servidor rodando em http://localhost:3000')
+//console.log('Servidor rodando em http://localhost:3000')
+
+
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send('Hello World Express!');
+});
+ 
+app.listen(3000);
+
