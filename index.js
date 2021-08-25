b8ab3b5d018bdd1e067235ec0e6c05fbe04f87f7
andersonmguedes <andersonm.guedes@gmail.com>
2021-08-25 00:42:14 -0300

    Servidor Node

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
-const calc = require('./calculadora');
-const prompt =require("prompt-sync")();
+const http =require('http');
 
-console.log(` Olá seja bem vindo a ${calc.nome}`);
+http.createServer((req,res)=>{}).listen(3000)
 
-const n1 = +prompt('Primeiro número:');
-const n2 = +prompt('Segundo número:');
-console.log()
-
-console.log(`
-Qual calculo voce quer?
-    [1] - soma
-    [2] - subtração
-    [3] - multiplicação
-    [4] - divisão
-`);
-const opcao = +prompt('Sua escolha: ');
-if (opcao ===1){
-    console.log(`${n1} + ${n2} = ${calc.soma(n1,n2)}`)
-}else if (opcao === 2) {
-    console.log(`${n1} - ${n2} = ${calc.sub(n1,n2)}`)
-}else if (opcao ===3) {
-    console.log(`${n1} * ${n2} = ${calc.mult(n1,n2)}`)
-}else if (opcao ===4) {
-    console.log(`${n1} / ${n2} = ${calc.div(n1,n2)}`)
-}else {
-    console.log('Opção Invalida!')
-}
+console.log('Servidor rodando em http://localhost:3000')
